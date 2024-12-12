6fe984ebe1f96482a80ac1b4b5bb0ac8f5e397f9
James108126 <stps108126@cb.stps.tp.edu.tw>
2024-12-12 20:39:50 +0800

    Update ex7-2.cpp

--- ex7-2.cpp
+++ ex7-2.cpp
@@ -46,15 +46,15 @@ int main() {
 
 
 
-  #include <iostream>
-#include <algorithm> 
+#include <iostream>
+#include <algorithm>  
 
 using namespace std;
 
 int main() {
     int num1, num2, num3, num4;
 
-  
+   
     cout << "請輸入第一個整數: ";
     cin >> num1;
     cout << "請輸入第二個整數: ";
@@ -64,19 +64,11 @@ int main() {
     cout << "請輸入第四個整數: ";
     cin >> num4;
 
-    
+  
     int min_num = min({num1, num2, num3, num4});
 
-
-  
-    cout << "四個整數中最小的數是: " << min_num << endl;
-
-    return 0;
-}
-
-
     
-    cout << "The smallest number among the four integers is: " << min_num << endl;
+    cout << "四個整數中最小的數是: " << min_num << endl;
 
     return 0;
 }
