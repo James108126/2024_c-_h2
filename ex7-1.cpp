a33d23158bcd08c9f3c5fd4a08f00b498ffa4759
James108126 <stps108126@cb.stps.tp.edu.tw>
2024-12-12 20:25:35 +0800

    Update ex7-1.cpp

--- ex7-1.cpp
+++ ex7-1.cpp
@@ -13,3 +13,32 @@
     
     return 0;
  }
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+void print_array(int a[]){
+    int i;
+    for(i=0;i<3;i++){
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+}
+ int main()
+ {
+    int i,a[3];
+    
+    for(i=0;i<3;i++){
+        a[i]=i+7;
+    }
+    for(i=0;i<3;i++){
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+    
+    return 0;
+ }
