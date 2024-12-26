57ed1bd1baf6cd30c01877867a7fa59f54239002
James108126 <stps108126@cb.stps.tp.edu.tw>
2024-12-26 19:18:27 +0800

    Create ex.9-1.cpp
    半成品

---
+++ ex.9-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    char greetinhs[ ] = "Hello Word!";
+    
+    printf("%s\n" , greetings);
+    
+    greetinhs[0] = "J";
+    
+    printf("%s\n" , greetings);
+    
+    greetinhs[4] = '4';  
+    
+    printf("%s\n" , greetings);
+    
+    return 0;
+}
